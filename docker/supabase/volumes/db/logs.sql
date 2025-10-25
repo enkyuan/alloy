6459e04a5e9c31e32fd74ae02c40f2c3ced3a7ff
enkyuan <yuan_enkang@outlook.com>
2025-10-25 00:40:05 -0400

    chore: wtf is docker doing

--- docker/supabase/volumes/db/logs.sql
+++ docker/supabase/volumes/db/logs.sql
@@ -1,3 +1,6 @@
-\c postgres
+\set pguser `echo "$POSTGRES_USER"`
+
+\c _supabase
 create schema if not exists _analytics;
-alter schema _analytics owner to supabase_admin;
+alter schema _analytics owner to :pguser;
+\c postgres
